d0d20890afebda84ba5baf83601d708fc5bc1fb8
Kemo Deschamps <kemo.deschamps@outlook.fr>
2022-12-09 17:05:18 +0100

    revert imageTraking

--- Scol-AR/Assets/Scripts/ImageTracking.cs
+++ Scol-AR/Assets/Scripts/ImageTracking.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -11,18 +8,22 @@ public class ImageTracking : MonoBehaviour
     [SerializeField]
     private GameObject[] placeablePrefab;
 
-    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
     private ARTrackedImageManager trackedImageManager;
 
     private void Awake()
     {
         trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+        GameObject newPrefab;
 
-        foreach(GameObject prefab in placeablePrefab)
+        foreach (GameObject prefab in placeablePrefab)
         {
-            GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
-            newPrefab.name = prefab.name;
-            spawnedPrefabs.Add(prefab.name, newPrefab);
+            if (!GlobalVariable.listAtom.ContainsKey(prefab.name))
+            {
+                newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+                newPrefab.name = prefab.name;
+                newPrefab.SetActive(false);
+                GlobalVariable.listAtom.Add(prefab.name, newPrefab);
+            }
         }
     }
     private void OnEnable()
@@ -48,7 +49,7 @@ public class ImageTracking : MonoBehaviour
 
         foreach (ARTrackedImage trackedImage in eventArgs.removed)
         {
-            spawnedPrefabs[trackedImage.name].SetActive(false);
+            GlobalVariable.listAtom[trackedImage.name].SetActive(false);
         }
     }
 
@@ -56,16 +57,18 @@ public class ImageTracking : MonoBehaviour
     {
         string name = trackedImage.referenceImage.name;
         Vector3 position = trackedImage.transform.position;
-        GameObject prefab = spawnedPrefabs[name]; 
+        GameObject prefab = GlobalVariable.listAtom[name];
 
         if (trackedImage.trackingState == TrackingState.Limited)
         {
             prefab.SetActive(false);
+            GlobalVariable.currentImages.Remove(name);
         }
-        else if(trackedImage.trackingState == TrackingState.Tracking)
+        else if (trackedImage.trackingState == TrackingState.Tracking)
         {
             prefab.transform.position = position;
             prefab.SetActive(true);
+            GlobalVariable.currentImages.Add(name);
         }
     }
 }
