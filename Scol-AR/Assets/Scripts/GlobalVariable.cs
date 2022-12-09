b85fbb4e41b77d3b3f5ad1c41e0ac49c82da8008
Arthur-Ponty <arthur.ponty76@gmail.com>
2022-12-09 16:04:04 +0100

    Some change

--- Scol-AR/Assets/Scripts/GlobalVariable.cs
+++ Scol-AR/Assets/Scripts/GlobalVariable.cs
@@ -2,23 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalVariable : MonoBehaviour
+public class GlobalVariable
 {
     [HideInInspector]
     public static Dictionary<string, GameObject> listAtom = new Dictionary<string, GameObject>();
     public static List<string> currentImages = new List<string>();
     public static List<string> moleculeFound = new List<string>();
-    public static List<string> periodicTable = new List<string>();
-
-    private void Start()
+    public static List<string> periodicTable = new List<string>()
     {
-        periodicTable.AddRange(new List<string>
-        {
-            "H", "He",
-            "Li", "Be", "B", "C", "N", "O", "F", "Ne",
-            "Na", "Mg", "Al", "Si", "P", "P", "S", "Cl", "Ar",
-            "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
-            "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe"
-        });
-    }
+        "H", "He",
+        "Li", "Be", "B", "C", "N", "O", "F", "Ne",
+        "Na", "Mg", "Al", "Si", "P", "P", "S", "Cl", "Ar",
+        "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
+        "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe"
+    };
 }
